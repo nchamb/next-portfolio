ab52d2ffb9c9309ed05af2e64b4f370c3e6a863d
nipunbandara <nipun.bandara@outlook.com>
2024-05-12 19:35:31 +1000

    exp

--- src/app/experience.tsx
+++ src/app/experience.tsx
@@ -9,11 +9,110 @@ import {
   Typography,
 } from "@material-tailwind/react";
 
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+
+const ProjectsSectionLarge = () => {
+  // Define the section content for larger screens
+  return (
+<div id = "experience" className="container mx-auto p-4">
+      <h1 className="text-2xl font-semibold mb-4">My Experience</h1>
+      <Timeline className="">
+      <div className="">
+      <time className=" translate-y-0.5 inline-flex items-center justify-center text-xs font-semibold uppercase w-26 h-6 mb-3 sm:mb-0 text-emerald-600 bg-emerald-100 rounded-full">Apr, 2023 - Jan, 2024</time>
+       
+        <TimelineItem className="float-right pr-48">
+          <TimelineConnector className="" />
+          <TimelineHeader className="h-3">
+            <TimelineIcon className=""/>
+            <Typography variant="h6" color="blue-gray" className="leading-none">
+              Software Engineer - Self-employed
+            </Typography>
+          </TimelineHeader>
+          <TimelineBody className="pb-8">
+            <div className="text-base font-bold font-caveat text-gray-600">CorsDeck Pvt Ltd</div>
+
+            <Typography variant="small" color="gary" className="font-normal text-gray-600">
+              Collaborated with product managers and stakeholders to gather requirements and to prioritize project development.<br/>
+              Implemented authentication and authorization mechanisms using JWT tokens and OAuth2.<br/>
+              Implemented caching strategies using React Quarry and Redux.<br/>
+              Developed custom integrations with third party APIs such as email providers, QR generators and payment gateways.<br/>
+              Architected and developed RESTful APIs, to seamless integration with systems.<br/>
+              Optimized databases and developed efficient API calls using GraphQL.<br/>
+              Deployed application using CI/CD tools with Docker, Kubernetes, AWS and GCP pipelines.<br/>
+              Resolved production issues and implemented unit testing using JUnit and Jest.
+            </Typography>
+          </TimelineBody>
+        </TimelineItem>
+
+        </div>
+        <div>
+        <time className=" translate-y-0.5 inline-flex items-center justify-center text-xs font-semibold uppercase w-26 h-6 mb-3 sm:mb-0 text-emerald-600 bg-emerald-100 rounded-full">Feb, 2019 - Dec, 2023</time>
+        <TimelineItem className="float-right pr-3 mr-80">
+          {/* <TimelineConnector className="pl-0.4 ml-24" /> */}
+          <TimelineHeader className="h-3">
+            <TimelineIcon className=""/>
+            <Typography variant="h6" color="blue-gray" className="leading-none">
+              Administrative Clerk - Part-time
+            </Typography>
+          </TimelineHeader>
+          <TimelineBody className="pb-8">
+            <div className="text-base font-bold font-caveat text-gray-600">Sunil Electricals</div>
+
+            <Typography variant="small" color="gary" className="font-normal text-gray-600">
+              Wrote and sent contracts, finalizing paperwork on behalf of staff.<br/>
+              Proofread and edited correspondence, reports and files.<br/>
+              Kept office file system highly organized and quickly retrieved needed records.<br/>
+              Maintained accurate accounting records, inputting new invoices, and payments into Database.<br/>
+              Answered email and mail correspondence on behalf of personnel.<br/>
+              Tracked inventory orders and usage to maintain accurate records.
+            </Typography>
+          </TimelineBody>
+        </TimelineItem>
+        </div>
+      </Timeline>
+      </div>
+  );
+};
+
+const ProjectsSectionSmall = () => {
+  // Define the section content for smaller screens
+  return (
+    <div className="container mx-auto p-4">
+       <h1 className="text-2xl font-semibold mb-4">My Experience</h1>
+         <div className="bg-white shadow-md rounded-md p-6 mb-4">
+           <h2 className="text-xl font-semibold mb-2"> Software Engineer - Self-employed</h2>
+           <p className="text-gray-600">CorsDeck Pvt Ltd</p>
+           <p className="text-gray-600">Apr, 2023 - Jan, 2024</p>
+           <p className="text-gray-700 mt-2">
+              Collaborated with product managers and stakeholders to gather requirements and to prioritize project development.<br/>
+              Implemented authentication and authorization mechanisms using JWT tokens and OAuth2.<br/>
+              Implemented caching strategies using React Quarry and Redux.<br/>
+              Developed custom integrations with third party APIs such as email providers, QR generators and payment gateways.<br/>
+              Architected and developed RESTful APIs, to seamless integration with systems.<br/>
+              Optimized databases and developed efficient API calls using GraphQL.<br/>
+              Deployed application using CI/CD tools with Docker, Kubernetes, AWS and GCP pipelines.<br/>
+              Resolved production issues and implemented unit testing using JUnit and Jest.
+           </p>
+         </div>
+         <div className="bg-white shadow-md rounded-md p-6 mb-4">
+           <h2 className="text-xl font-semibold mb-2">Administrative Clerk - Part-time</h2>
+           <p className="text-gray-600">Sunil Electricals</p>
+           <p className="text-gray-600">Feb, 2019 - Dec, 2023</p>
+           <p className="text-gray-700 mt-2">
+              Wrote and sent contracts, finalizing paperwork on behalf of staff.<br/>
+              Proofread and edited correspondence, reports and files.<br/>
+              Kept office file system highly organized and quickly retrieved needed records.<br/>
+              Maintained accurate accounting records, inputting new invoices, and payments into Database.<br/>
+              Answered email and mail correspondence on behalf of personnel.<br/>
+              Tracked inventory orders and usage to maintain accurate records.
+           </p>
+         </div>
+        </div>
+  );
+};
 
 const Experience = () => {
-  return (
-   
+
 //   <div className="relative font-inter antialiased">
 
 //     <div className="relative min-h-screen flex flex-col justify-center bg-slate-50 overflow-hidden">
@@ -70,64 +169,115 @@ const Experience = () => {
 //       </div>
 //     </div>
     
-    <div id = "experience" className="container mx-auto p-4">
-      <h1 className="text-2xl font-semibold mb-4">My Experience</h1>
-      <Timeline className="">
-      <div className="">
-      <time className=" translate-y-0.5 inline-flex items-center justify-center text-xs font-semibold uppercase w-26 h-6 mb-3 sm:mb-0 text-emerald-600 bg-emerald-100 rounded-full">Apr, 2023 - Jan, 2024</time>
+    // <div id = "experience" className="container mx-auto p-4">
+    //   <h1 className="text-2xl font-semibold mb-4">My Experience</h1>
+    //   <Timeline className="">
+    //   <div className="">
+    //   <time className=" translate-y-0.5 inline-flex items-center justify-center text-xs font-semibold uppercase w-26 h-6 mb-3 sm:mb-0 text-emerald-600 bg-emerald-100 rounded-full">Apr, 2023 - Jan, 2024</time>
        
-        <TimelineItem className="float-right pr-48">
-          <TimelineConnector className="" />
-          <TimelineHeader className="h-3">
-            <TimelineIcon className=""/>
-            <Typography variant="h6" color="blue-gray" className="leading-none">
-              Software Engineer - Self-employed
-            </Typography>
-          </TimelineHeader>
-          <TimelineBody className="pb-8">
-            <div className="text-base font-bold font-caveat text-gray-600">CorsDeck Pvt Ltd</div>
+    //     <TimelineItem className="float-right pr-48">
+    //       <TimelineConnector className="" />
+    //       <TimelineHeader className="h-3">
+    //         <TimelineIcon className=""/>
+    //         <Typography variant="h6" color="blue-gray" className="leading-none">
+    //           Software Engineer - Self-employed
+    //         </Typography>
+    //       </TimelineHeader>
+    //       <TimelineBody className="pb-8">
+    //         <div className="text-base font-bold font-caveat text-gray-600">CorsDeck Pvt Ltd</div>
 
-            <Typography variant="small" color="gary" className="font-normal text-gray-600">
-              Collaborated with product managers and stakeholders to gather requirements and to prioritize project development.<br/>
-              Implemented authentication and authorization mechanisms using JWT tokens and OAuth2.<br/>
-              Implemented caching strategies using React Quarry and Redux.<br/>
-              Developed custom integrations with third party APIs such as email providers, QR generators and payment gateways.<br/>
-              Architected and developed RESTful APIs, to seamless integration with systems.<br/>
-              Optimized databases and developed efficient API calls using GraphQL.<br/>
-              Deployed application using CI/CD tools with Docker, Kubernetes, AWS and GCP pipelines.<br/>
-              Resolved production issues and implemented unit testing using JUnit and Jest.
-            </Typography>
-          </TimelineBody>
-        </TimelineItem>
+    //         <Typography variant="small" color="gary" className="font-normal text-gray-600">
+    //           Collaborated with product managers and stakeholders to gather requirements and to prioritize project development.<br/>
+    //           Implemented authentication and authorization mechanisms using JWT tokens and OAuth2.<br/>
+    //           Implemented caching strategies using React Quarry and Redux.<br/>
+    //           Developed custom integrations with third party APIs such as email providers, QR generators and payment gateways.<br/>
+    //           Architected and developed RESTful APIs, to seamless integration with systems.<br/>
+    //           Optimized databases and developed efficient API calls using GraphQL.<br/>
+    //           Deployed application using CI/CD tools with Docker, Kubernetes, AWS and GCP pipelines.<br/>
+    //           Resolved production issues and implemented unit testing using JUnit and Jest.
+    //         </Typography>
+    //       </TimelineBody>
+    //     </TimelineItem>
 
-        </div>
-        <div>
-        <time className=" translate-y-0.5 inline-flex items-center justify-center text-xs font-semibold uppercase w-26 h-6 mb-3 sm:mb-0 text-emerald-600 bg-emerald-100 rounded-full">Feb, 2019 - Dec, 2023</time>
-        <TimelineItem className="float-right pr-3 mr-80">
-          {/* <TimelineConnector className="pl-0.4 ml-24" /> */}
-          <TimelineHeader className="h-3">
-            <TimelineIcon className=""/>
-            <Typography variant="h6" color="blue-gray" className="leading-none">
-              Administrative Clerk - Part-time
-            </Typography>
-          </TimelineHeader>
-          <TimelineBody className="pb-8">
-            <div className="text-base font-bold font-caveat text-gray-600">Sunil Electricals</div>
+    //     </div>
+    //     <div>
+    //     <time className=" translate-y-0.5 inline-flex items-center justify-center text-xs font-semibold uppercase w-26 h-6 mb-3 sm:mb-0 text-emerald-600 bg-emerald-100 rounded-full">Feb, 2019 - Dec, 2023</time>
+    //     <TimelineItem className="float-right pr-3 mr-80">
+    //       {/* <TimelineConnector className="pl-0.4 ml-24" /> */}
+    //       <TimelineHeader className="h-3">
+    //         <TimelineIcon className=""/>
+    //         <Typography variant="h6" color="blue-gray" className="leading-none">
+    //           Administrative Clerk - Part-time
+    //         </Typography>
+    //       </TimelineHeader>
+    //       <TimelineBody className="pb-8">
+    //         <div className="text-base font-bold font-caveat text-gray-600">Sunil Electricals</div>
 
-            <Typography variant="small" color="gary" className="font-normal text-gray-600">
-              Wrote and sent contracts, finalizing paperwork on behalf of staff.<br/>
-              Proofread and edited correspondence, reports and files.<br/>
-              Kept office file system highly organized and quickly retrieved needed records.<br/>
-              Maintained accurate accounting records, inputting new invoices, and payments into Database.<br/>
-              Answered email and mail correspondence on behalf of personnel.<br/>
-              Tracked inventory orders and usage to maintain accurate records.
-            </Typography>
-          </TimelineBody>
-        </TimelineItem>
-        </div>
-      </Timeline>
-    </div>
-  );
-};
+    //         <Typography variant="small" color="gary" className="font-normal text-gray-600">
+    //           Wrote and sent contracts, finalizing paperwork on behalf of staff.<br/>
+    //           Proofread and edited correspondence, reports and files.<br/>
+    //           Kept office file system highly organized and quickly retrieved needed records.<br/>
+    //           Maintained accurate accounting records, inputting new invoices, and payments into Database.<br/>
+    //           Answered email and mail correspondence on behalf of personnel.<br/>
+    //           Tracked inventory orders and usage to maintain accurate records.
+    //         </Typography>
+    //       </TimelineBody>
+    //     </TimelineItem>
+    //     </div>
+    //   </Timeline>
+
+    //   <style jsx>{`
+    //     @media (max-width: 499px) {
+    //       .text-sm {
+    //         font-size: 12px;
+    //       }
+    //       .lg\:text-lg {
+    //         font-size: 14px;
+    //       }
+    //     }
+    //   `}</style>
+    //   @media (max-width: 640px) {
+    //      <div className="container mx-auto p-4">
+    //      <div className="bg-white shadow-md rounded-md p-6 mb-4">
+    //        <h2 className="text-xl font-semibold mb-2">Research Assistant</h2>
+    //        <p className="text-gray-600">University of Research</p>
+    //        <p className="text-gray-600">Sept 2018 - Dec 2019</p>
+    //        <p className="text-gray-700 mt-2">Contributed to multiple research projects in the field of Artificial Intelligence, conducted experiments, and analyzed data. Published several papers in reputable conferences.</p>
+    //      </div>
+    //      <div className="bg-white shadow-md rounded-md p-6 mb-4">
+    //        <h2 className="text-xl font-semibold mb-2">Intern</h2>
+    //        <p className="text-gray-600">XYZ Corporation</p>
+    //        <p className="text-gray-600">May 2019 - Aug 2019</p>
+    //        <p className="text-gray-700 mt-2">Assisted in the development of mobile applications using React Native, gained experience in Agile methodologies.</p>
+    //      </div>
+    //      <div className="bg-white shadow-md rounded-md p-6 mb-4">
+    //        <h2 className="text-xl font-semibold mb-2">Software Engineer</h2>
+    //        <p className="text-gray-600">ABC Tech Inc.</p>
+    //        <p className="text-gray-600">Jan 2020 - Present</p>
+    //        <p className="text-gray-700 mt-2">Developed web applications using React and Node.js, collaborated with cross-functional teams to deliver projects on time.</p>
+    //      </div>
+    //      <h1 className="text-2xl font-semibold mb-4">My Experience</h1>
+    //    </div>
+    //   }
+    // </div>
+    const [isSmallScreen, setIsSmallScreen] = useState(false);
+
+    // Function to update isSmallScreen state based on screen width
+    const handleResize = () => {
+      setIsSmallScreen(window.innerWidth < 900);
+    };
+  
+    // Effect hook to listen for window resize events
+    useEffect(() => {
+      window.addEventListener('resize', handleResize);
+      // Cleanup function to remove event listener when component unmounts
+      return () => {
+        window.removeEventListener('resize', handleResize);
+      };
+    }, []); // Empty dependency array to run effect only once on component mount
+  
+    // Render different sections based on screen width
+    return isSmallScreen ? <ProjectsSectionSmall /> : <ProjectsSectionLarge />;
+  };
 
 export default Experience;
